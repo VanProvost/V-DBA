d2b04d4a931d408c43f63f5ad37ddde7bf848776
Van Provost <vanprovostbusiness@gmail.com>
2025-03-24 18:13:16 -0400

    Updating gitignore

--- Midterm-Project/Queries Midterm.sql
+++ Midterm-Project/Queries Midterm.sql
@@ -1,33 +1,43 @@
-SELECT vendor_id, concat(vendors.contact_first_name, ' ', vendors.contact_last_name) as 'Full Name', phone			# this concatinates first name and last name as Full name. 
+# this concatinates first name and last name as Full name. 
+SELECT vendor_id, concat(vendors.contact_first_name, ' ', vendors.contact_last_name) as 'Full Name', phone			
 FROM vendors; 
 
+#select statement
 SELECT book_id,author_id, title, genre, price
 FROM books; 
 
-select *
-from orders
-where status = 'paid';			#will grab everything from orders if the status is = to paid 
 
-select *
-from payments;
-
-#join 
-select	orders.order_id	,orders.order_date, payments.payment_date, payments.payment_method			#Joins payments and orders into one table 
-from orders 
-left join payments ON orders.order_id=payments.order_id;
+#will grab everything from orders if the status is = to paid 
+SELECT *
+FROM orders
+WHERE STATUS = 'paid';			
 
 
-SELECT SUM(invoice_total) AS total_invoice_amount			#sums invoice total
+#Joins payments and orders into one table 
+SELECT	orders.order_id	,orders.order_date, payments.payment_date, payments.payment_method			
+FROM orders 
+LEFT JOIN payments ON orders.order_id=payments.order_id;
+
+#sums invoice total
+SELECT SUM(invoice_total) AS total_invoice_amount			
 FROM invoices; 
 
+#Counts all orders from order_id where the order is either returned or refunded
 SELECT count(*) order_id
-FROM orders;
+FROM orders
+WHERE status = 'Returned' OR 'Refunded';
 
-SELECT invoice_id, vendor_id, sum(invoice_total) AS total_revenue, 				#sums invoice_total as total_revenue 
-		GROUPING(invoice_id) AS group_invoice_id,								# Grouping is used to indicate what collumns have been aggregated in subtotal and grand total rows. when invoice_id/vendor_id grouping returns a 1, the value is part of an aggregate row. 
-        GROUPING(vendor_id) AS group_vendor_id
+SELECT 
+    vendor_id, 
+    SUM(invoice_total) AS total_revenue,
+    GROUPING(invoice_id) AS group_invoice_id
 FROM invoices
-GROUP BY invoice_id, vendor_id WITH ROLLUP;
+GROUP BY vendor_id, invoice_id
+WITH ROLLUP
+HAVING GROUPING(invoice_id) = 1
+ORDER BY vendor_id;
+
+
 
 
 
